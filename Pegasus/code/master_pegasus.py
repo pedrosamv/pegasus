a79ddfe214ed9844e05a8f94059d03944b50c5ba
pedrosamv <pedrosamariaveronica@gmail.com>
2026-06-07 12:53:48 -0300

    Refactor master_pegasus.py to improve structure

--- Pegasus/code/master_pegasus.py
+++ Pegasus/code/master_pegasus.py
@@ -25,7 +25,7 @@ code = r"C:\Users\usuario\Dropbox\Python\Pegasus\code"
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP],
                 suppress_callback_exceptions=True)
-
+server = app.server
 app.index_string = '''
 <!DOCTYPE html>
 <html>
